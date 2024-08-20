459bcbb77407dffda6e2ff08c5b3dbff9320e2e6
Paulo Cavalcante <ph.doamaralcavalcante@gmail.com>
2024-08-20 20:47:59 -0300

    feat and fix: changes done and bugs fixed

--- js/script.js
+++ js/script.js
@@ -8,3 +8,30 @@ const toggleNav = () => {
   mobileNav.classList.toggle("hamburger-active");
 };
 mobileNav.addEventListener("click", () => toggleNav());
+
+const numbers = document.querySelectorAll(".count");
+
+if (numbers.length > 0) {
+  const startNumberAnimation = (element) => {
+    const number = +element.getAttribute('data-number');
+    const numberDivision = number / 30;
+    const animationRuntimeMS = 50;
+    let dynamicNumber = 0;
+
+    element.innerText = dynamicNumber;
+
+    const animateNumbers = setInterval(() => {
+      if (dynamicNumber < number) {
+        dynamicNumber += numberDivision;
+        element.innerText = Math.floor(dynamicNumber);
+      } else {
+        element.innerText = number;
+        clearInterval(animateNumbers);
+      }
+    }, Math.random() * animationRuntimeMS);
+  };
+
+  numbers.forEach((number) => {
+    startNumberAnimation(number);
+  });
+}
